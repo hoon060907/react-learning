119bc25e4c103b711b749c2906fc464a8f5590c9
gihoon <gihoon0907@gmail.com>
2023-06-06 23:51:46 +0900

    second commit

--- src/Say.js
+++ src/Say.js
@@ -14,8 +14,8 @@ const Say = () => {
             <button style={{ color: 'red' }} onClick={() => setColor('red')}>
                 빨간색
             </button>
-            <button style={{ color: 'green' }} onClick={() => setColor('green')}>
-                초록색
+            <button style={{ color: 'yellow' }} onClick={() => setColor('yellow')}>
+                노란색
             </button>
             <button style={{ color: 'blue' }} onClick={() => setColor('blue')}>
                 파란색
