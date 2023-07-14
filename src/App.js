50c4b1276e56e08a527ab9202027d4563661cb4b
gihoon <gihoon0907@gmail.com>
2023-07-14 11:13:22 +0900

    iteration

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import { Component } from "react";
-import ValidationSample from "./ValidationSample";
+import IterationSample from "./IterationSample";
+
 class App extends Component {
   render() {
     return (
-      <ValidationSample/>
+      <IterationSample/>
     );
   }
 }
